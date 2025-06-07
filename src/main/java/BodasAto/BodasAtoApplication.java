c7b484c5045724a9a90632923d6836f4da266575
AdrianAto1 <adrianklinsmann.s388544@cesurformacion.com>
2025-06-07 11:40:47 +0200

    new changes

--- src/main/java/BodasAto/BodasAtoApplication.java
+++ src/main/java/BodasAto/BodasAtoApplication.java
@@ -3,6 +3,9 @@ package BodasAto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
 import jakarta.annotation.PostConstruct;
 
 
@@ -15,10 +18,15 @@ public class BodasAtoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BodasAtoApplication.class, args);
+        System.out.println("App iniciada y main thread terminado");
     }
 
     @PostConstruct
     public void init() {
         System.out.println("### La aplicación se está ejecutando en el puerto: " + port);
+    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void onReady() {
+        System.out.println("App lista para recibir solicitudes");
     }
 }
