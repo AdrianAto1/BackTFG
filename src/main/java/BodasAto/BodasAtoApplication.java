2ae9e0ee204167f6f3ddc4962e82772ab3030c95
AdrianAto1 <adrianklinsmann.s388544@cesurformacion.com>
2025-06-07 11:31:31 +0200

    version 2

--- src/main/java/BodasAto/BodasAtoApplication.java
+++ src/main/java/BodasAto/BodasAtoApplication.java
@@ -1,12 +1,24 @@
 package BodasAto;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+
+
 
 @SpringBootApplication
 public class BodasAtoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BodasAtoApplication.class, args);
-	}
 
+    @Value("${server.port}")
+    private String port;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BodasAtoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("### La aplicación se está ejecutando en el puerto: " + port);
+    }
 }
